705fe848c6cbc32f8d1d1b855395bc28a9c1f0f9
Russel Rai <2011869@brunel.ac.uk>
2021-12-14 15:36:29 +0000

    Organise ship.java

--- src/main/java/Ship.java
+++ src/main/java/Ship.java
@@ -12,19 +12,10 @@ public class Ship {
     private final String symbol;
 
     private final int length;
-    private int intactParts;
-
-    private static int hitPoints; // all ships will share same hitPoints
-    private static int score = 0; // all ships will share same score
-
-    private boolean sunk;
-    private boolean gotHit;
 
     public Ship(String name, int length) {
         this.name = name;
         this.length = length;
-        this.intactParts = getLength();
-        hitPoints += getLength();
 
         switch (name) {// sets the symbol for ships in initialisation
             case "Patrol Boat":
@@ -58,61 +49,14 @@ public class Ship {
         return symbol;
     }
 
-    public int getHitPoints() {
-        return hitPoints;
-    }
+//    public String sunkenReport() {
+//        return "You sank my " + getName() + "\n";
+//    }
 
-    public int getScore() {
-        return score;
-    }
-
-    public void resetHitpoints() {
-        hitPoints = 0;
-    }
-
-    public void hit() { // call this when ship is hit
-        intactParts--;
-        hitPoints--;
-        gotHit = true;
-        score++;
-    }
-
-    public void missed() {
-        if (score <= 0) {
-            score = 0;
-        } else {
-            score--;
-        }
-    }
-
-    public boolean sunken() { // check if ship is sunken
-        if (intactParts == 0) {
-            sunk = true;
-            score += getLength() * 2; // e.g. 5 * 2 = 10pts is added
-            return true;
-        }
-        return false;
-    }
-
-    public boolean isSunk() {
-        return sunk;
-    }
-    public boolean isHit() {
-        return gotHit;
-    }
-
-    public String sunkenReport() {
-        return "You sank my " + getName() + "\n";
-    }
-
-    public void resetScore() {
-        score = 0;
-    }
-
-    public String getStatus() {
-        return "\nStatus >> HP: " + hitPoints +
-            ", Score: " + score;
-    }
+//    public String getStatus() {
+//        return "\nStatus >> HP: " + hitPoints +
+//            ", Score: " + score;
+//    }
 
     @Override
     public String toString() {
